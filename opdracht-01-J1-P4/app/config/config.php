be717f124278704869f2455a7d91501c64ff7e47
timdegroot05 <336795@student.mboutrecht.nl>
2023-05-07 15:05:57 +0200

    first commit

---
+++ opdracht-01-J1-P4/app/config/config.php
@@ -0,0 +1,8 @@
+<?php
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+//define('DB_NAME', 'mvc-2209a');
+define('DB_NAME', 'Mvc-2209a-P4');
+define('URLROOT', 'http://opdracht-1-p4/');
