3062b77e6b2c47c731c9d48f5f8aae4d64b69bc9
timdegroot05 <336795@student.mboutrecht.nl>
2023-05-08 21:15:15 +0200

    commit

--- opdracht-01-J1-P4/app/db/queries.sql
+++ opdracht-01-J1-P4/app/db/queries.sql
@@ -1,6 +1,8 @@
-SELECT      VoertuigInstructeur.*"
-            ,Voertuig."
-FROM        VoertuigInstructeur AS VI
-INNER JOIN  Voertuig AS VoertuigInstructeur
-ON          VoertuigInstructeur.VoertuigId = Voertuig.Id
-WHERE       InstructeurId = 1;
+SELECT          VoertuigInstructeur.*"
+                ,Voertuig."
+FROM            VoertuigInstructeur AS VI
+INNER JOIN      Voertuig AS VoertuigInstructeur
+ON              VI.VoertuigId = VO.Id
+INNER JOIN      TypeVoertuig AS TV
+ON              VO.typeVoertuigId = TV.Id
+WHERE           InstructeurId = 1;
